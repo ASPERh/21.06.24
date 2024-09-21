c48ec9150f34fdf1c68fc1a9af80ce44f76ddb3f
ASPER <144143767+ASPERh@users.noreply.github.com>
2024-09-21 13:11:37 +0100

    Add files via upload

---
+++ 21.06.24/21.06.24/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 5;
+
+            char symbol = '@';
+
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(' ');
+
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            if (x > 0) x--;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (x < width - 1) x++;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (y > 0) y--;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (y < height - 1) y++;
+                            break;
+                    }
+
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(symbol);
+                }
+
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
